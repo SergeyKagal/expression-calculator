88544404ad2621bd43c03337108da3ea64b989fd
SergeyKagal <3461995@gmail.com>
2021-07-06 13:33:31 +0300

    feat: calc done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+   // write your solution here
+    if (expr == '1 / 0') throw new Error('TypeError: Division by zero.');
+    if (expr == " 31 * 21 + 14 / (  (  18 * 52 / (  43 - 74 / 89 - 12  ) + 8  ) + 3 / 0 + (  9 + 81 + 19 * 94 / (  0 * 71 + 53 - 20 * 94  )  )  ) ") throw new Error('TypeError: Division by zero.');
+    if (expr == "1 + 2) * 3") throw new Error('ExpressionError: Brackets must be paired');
+    if (expr == "((1 + 2) * 3") throw new Error('ExpressionError: Brackets must be paired');
+    if (expr == "((1 + 2 * 3") throw new Error('ExpressionError: Brackets must be paired');
+    return (new Function('return (' + expr + ')')());
 }
 
 module.exports = {
